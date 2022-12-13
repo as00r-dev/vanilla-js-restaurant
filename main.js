48e2e10308d492adbe5fd99e1e83e59de94443c1
as00r <as00r.dev@gmail.com>
2022-12-14 01:48:27 +0530

    Add HTML plan

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("Hi! from src/index.js but bundled in dist/main.js");
+
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdmFuaWxsYS1qcy1yZXN0YXVyYW50Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiSGkhIGZyb20gc3JjL2luZGV4LmpzIGJ1dCBidW5kbGVkIGluIGRpc3QvbWFpbi5qc1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
