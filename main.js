f1860a9dae2384a75b4550c7ac32c2766a556fa7
as00r <as00r.dev@gmail.com>
2022-12-13 21:23:03 +0530

    Add dev-mode, source map, HtmlWebpackPlugin

---
+++ main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hi! from src/index.js but bundled in dist/main.js");
+console.log("I did some changes");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly92YW5pbGxhLWpzLXJlc3RhdXJhbnQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIaSEgZnJvbSBzcmMvaW5kZXguanMgYnV0IGJ1bmRsZWQgaW4gZGlzdC9tYWluLmpzXCIpO1xuY29uc29sZS5sb2coXCJJIGRpZCBzb21lIGNoYW5nZXNcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
