111f90ffd93517d2357f2613ca9eb3b968e02a0f
as00r <as00r.dev@gmail.com>
2022-12-15 01:32:07 +0530

    Add responsive header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
 console.log("Hello");
+const nav = document.querySelector("nav");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+	const isVisible = nav.getAttribute("data-visible");
+	if (isVisible === "true") {
+		nav.classList.add("transition");
+		nav.setAttribute("data-visible", "false");
+		hamburger.setAttribute("aria-expanded", "false");
+	} else {
+		nav.classList.add("transition");
+		nav.setAttribute("data-visible", "true");
+		hamburger.setAttribute("aria-expanded", "true");
+	}
+});
